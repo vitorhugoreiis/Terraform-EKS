cf712df03f6e744cee2aabb837531aa273061de0
Vitor Hugo <vitorhugoreiis@gmail.com>
2024-03-08 14:51:21 -0300

    add internet gateway and  route table

--- public.tf
+++ public.tf
@@ -39,3 +39,18 @@ resource "aws_subnet" "eks_subnet_public_1c" {
     }
   )
 }
+
+resource "aws_route_table_association" "eks_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_1c" {
+  subnet_id      = aws_subnet.eks_subnet_public_1c.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
