bd64db6b595c3dab05d6cf9af136e0b16fe52382
Vitor Hugo <vitorhugoreiis@gmail.com>
2024-03-09 10:58:45 -0300

    IAM load balancer controller

--- modules.tf
+++ modules.tf
@@ -28,4 +28,5 @@ module "eks_load_balancer_controller" {
   source       = "./modules/aws-load-balancer-controller"
   project_name = var.project_name
   tags         = local.tags
+  oidc         = module.eks_cluster.oidc
 }
