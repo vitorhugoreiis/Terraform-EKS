5b81e891e47c0b2eec2c5668d2f02282a6bb58d7
Vitor Hugo <vitorhugoreiis@gmail.com>
2024-03-09 15:16:12 -0300

    add

--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
-output "oidc" {
-  value = module.eks_cluster.oidc
-}
+# output "oidc" {
+#   value = module.eks_cluster.oidc
+# }
+
+# output "ca" {
+#   value = module.eks_cluster.certificate_authority[0].data
+# }
+
+
+# output "endpoint" {
+#   value = module.eks_cluster.endpoint
+# }
