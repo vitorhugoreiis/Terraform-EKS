de0c4e4a34f7c710d41091bf181e3efbf69ceb20
Vitor Hugo <vitorhugoreiis@gmail.com>
2024-03-09 01:14:13 -0300

    add node group

--- modules.tf
+++ modules.tf
@@ -13,3 +13,13 @@ module "eks_cluster" {
   public_subnet_1b = module.eks_network.subnet_pub_1b
   public_subnet_1c = module.eks_network.subnet_pub_1c
 }
+
+module "eks_managed_node_group" {
+  source            = "./modules/managed-node-group"
+  project_name      = var.project_name
+  cluster_name      = module.eks_cluster.cluster_name
+  subnet_private_1a = module.eks_network.subnet_priv_1a
+  subnet_private_1b = module.eks_network.subnet_priv_1b
+  subnet_private_1c = module.eks_network.subnet_priv_1c
+  tags              = local.tags
+}
