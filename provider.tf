4aa0adf320eed956414d8fd74ca9a044e0bf824b
Vitor Hugo <vitorhugoreiis@gmail.com>
2024-03-10 15:37:22 -0300

    add helm load balancer

--- provider.tf
+++ provider.tf
@@ -40,7 +40,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
       command     = "aws"
     }
   }
--- variables.tf
+++ variables.tf
@@ -7,8 +7,3 @@ variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (Name tag)"
 }
-
-variable "cluster_name" {
-  type    = string
-  default = "Eks-Development-cluster"
-}
